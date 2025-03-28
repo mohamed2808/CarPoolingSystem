b0d3a849b62345408e5541a6577412930a73d1c6
Hagag <mohamedabohagag54@gmail.com>
2025-03-29 00:31:00 +0200

    Add Entities

---
+++ CarPoolingSystem.DataAccess/Entites/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPoolingSystem.DataAccess.Entites
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string Role { get; set; } 
+
+        public double Rating { get; set; }
+    }
+}
